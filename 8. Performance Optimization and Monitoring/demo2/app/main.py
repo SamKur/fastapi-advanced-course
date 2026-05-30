c8115c41e902803a6179c990e09cd94c32d5b223
Susamay Kumbhakar <susamay.sk@gmail.com>
2026-05-30 20:07:23 +0530

    modified prom grafana projects

--- 8. Performance Optimization and Monitoring/demo2/app/main.py
+++ 8. Performance Optimization and Monitoring/demo2/app/main.py
@@ -5,6 +5,8 @@ app = FastAPI()
 
 Instrumentator().instrument(app).expose(app)
 
+# in grafana ui ie 3000 port add data source as docker_service_or_img_name:9090 like prometheus:9090
+# not localhost:9090 because Docker handles networking and isolation. localhost would point to grafana's port
 
 @app.get('/')
 def root():
